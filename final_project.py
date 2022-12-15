c58b3cd1f6b78349e97937c58e9f0189fd9a2361
Sijia11 <106551020+Sijia11@users.noreply.github.com>
2022-12-15 18:13:56 -0500

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -1,47 +1,56 @@
-#FlaskInputApp.py
 from flask import Flask, render_template, request
 app = Flask(__name__)
 import json
 import requests
-#from requests_oauthlib import OAuth1
+
 
 import secrets_key
 from collections import defaultdict
+import create_json
+import plotly.graph_objects as go
+import read_json_tree
+
 map_find_place_params = {"fields" : "formatted_address,name,rating,opening_hours,geometry,type", "inputtype" : "textquery" }
 text_search_params = {'query': 'dark sky park', "type": "park", "radius": "10000"}
 weather_params = {}
+distance_params = {}
 global INFO, LOCATION
 @app.route('/')
 def index():
     
-    return render_template('FlaskInputs2.html'),"aa" # just the static HTML
+    return render_template('Home.html') # just the static HTML
     
 
 @app.route('/handle_form', methods=['POST'])
 def handle_the_form():
-    user_name = request.form["location"]
-    fave_color = request.form["colors"]
-    map = request.form["map"]
-    result_list = []
-    result_list.append(map)
-    answer = ask_map(mapTree, result_list,0)
-    return render_template('response.html', 
-        name=user_name, 
-        color=fave_color, map=map, answer=answer, info="1", ideal="1")
-
-
-@app.route('/search_map_form', methods=['POST'])
-def handle_the_search():
     global INFO, LOCATION
-    if "start" in request.form:
-        start_location = request.form["start"]
-        map_find_place_params["input"] = start_location
-        print(start_location)
+    search = request.form["search_type"]
+    location = request.form["location"]
+    distance = request.form["distance"]
+    cloud = request.form["cloud"]
+    option = request.form["option"]
+    result_list = []
+    if distance != "3":
+        result_list.extend([search, location, distance,cloud])
+    if distance == "3":
+        result_list.extend([search, location,cloud])
+    # print(result_list)
+    answer, final_request = create_json.ask_map(create_json.mapTree, result_list,0)
+    create_json.save_json("json_tree.json",answer)
+    res,new_tree = read_json_tree.read_json("json_tree.json")
+    # print(new_tree)
+
+    if search == "1":
+ 
+        map_find_place_params["input"] = location
+       # print(start_location)
         res = make_request_with_cache(map_find_place_url, map_find_place_params, "map")
-        print(type(res))
+     
         lat = res["candidates"][0]["geometry"]["location"]["lat"]
         lng = res["candidates"][0]["geometry"]["location"]["lng"]
         text_search_params["location"] = str(lat) + "," + str(lng)
+        distance_params["origins"] = str(lat) + "," + str(lng)
+   
         result = make_request_with_cache(map_endpoint_url, text_search_params, "map")["results"]
         info = []
         location = []
@@ -55,56 +64,85 @@ def handle_the_search():
         INFO = info
         LOCATION = location
 
+        
+        dropped_location = []
+        for lat, lng, i in LOCATION:
+            distance_params["destinations"] = str(lat) + "," + str(lng)
+            result = make_request_with_cache(distance_url, distance_params, "map")["rows"][0]["elements"][0]["distance"]["value"]
+          
+            INFO[i] += f", distance: {result//1609}"
+            if result > 563.27*1000:
+                dropped_location.append(i)
 
+   
+        if distance == "1":
+            for i in range(len(dropped_location)-1,-1,-1):
+                LOCATION.pop(dropped_location[i])
+                INFO.pop(dropped_location[i])
 
+    if search == "2":
 
-    if "park" in request.form:
-        park = request.form["park"]
-        map_find_place_params["input"] = park
+        map_find_place_params["input"] = location
         result = make_request_with_cache(map_find_place_url, map_find_place_params, "map")["candidates"]
         info = []
         location = []
         for i, val in enumerate(result):
             address = val["formatted_address"]
             name = val["name"]
-            info += [f"address:{address}, park name:{name}"]
+            info += [f"address:{address}, park name:{name}, distance: n/a"]
             location.append([val["geometry"]["location"]["lat"],val["geometry"]["location"]["lng"], i])
         INFO = info
         LOCATION = location
 
+    if cloud == "1":
+        cloudiness = 20
+    elif cloud == "2":
+        cloudiness = 10
 
-    return render_template('response1.html', 
-        
-        result=info)
-
-@app.route("/handle_weather", methods=["POST"])
-def handle_weather():
-    cloud = request.form["cloud"]
-    result_list = []
-    result_list.append(cloud)
-    answer = ask_map(weatherTree, result_list,0)
     weather_params = {}
     ideal = defaultdict(list)
-    for lat, lng, i in LOCATION:
-        weather_params["lat"] = lat
-        weather_params["lon"] = lng
-        a = make_request_with_cache(weather_endpoint_url, weather_params, "weather")
-        print(a)
+    print(len(INFO),len(LOCATION),"llllllll")
+    for i,v in enumerate(LOCATION):
+        print(i)
+        weather_params["lat"] = v[0]
+        weather_params["lon"] = v[1]
+
         result = make_request_with_cache(weather_endpoint_url, weather_params, "weather")["list"]
 
         
         for val in result:
-            if val["dt_txt"][11:13] == "21" and val["clouds"]["all"] < 20:
+            if val["dt_txt"][11:13] in ["21","00","03"] and val["clouds"]["all"] < cloudiness:
                 ideal[INFO[i]].append([val["dt_txt"], val["clouds"]["all"] ])
                 break
+
+    unsorted_list = []
+
+    for k,v in ideal.items():
+        sublist = []
+        idx = k.find("park name")
+        idx2 = k.find("distance")
+        sublist.append(k[8:idx-2]) # address
+        sublist.append(k[idx+10:idx2-2]) # name
+        sublist.append(k[idx2+10:]) # distance
+        sublist.append(v[0][0][:10]) # date
+        sublist.append(v[0][0][11:]) # time
+        sublist.append(v[0][1]) # cloud
+        unsorted_list.append(sublist)
     
-                
+    sorted_list = sorted(unsorted_list, key=lambda x:int(x[int(option)+1]) if str(x[int(option)+1]).isnumeric() else x[int(option)+1])
 
+    total_list = [[] for i in range(6)]
+    for info in sorted_list:
+        for i in range(len(info)):
+            total_list[i].append(info[i])
 
-
-
-    return render_template('response.html', answer=answer, info=INFO, ideal=ideal)
-
+    header = ["Address","Park name","Distance (mile)","Date", "Time", "Cloudiness (%)"]
+    fig = go.Figure(data=[go.Table(header=dict(values=header),
+                 cells=dict(values=total_list))
+                     ])
+    div = fig.to_html(full_html=False)
+    
+    return render_template('response.html', answer=final_request, info=INFO, ideal=ideal,plot_div=div)
 
 
 CACHE_FILENAME = "final_project_cache.json"
@@ -113,13 +151,7 @@ CACHE_DICT = {}
 map_key = secrets_key.MAP_API_KEY
 weather_key = secrets_key.WEATHER_API_KEY
 
-# access_token = secrets.TWITTER_ACCESS_TOKEN
-# access_token_secret = secrets.TWITTER_ACCESS_TOKEN_SECRET
 
-# oauth = OAuth1(client_key,
-#             client_secret=client_secret,
-#             resource_owner_key=access_token,
-#             resource_owner_secret=access_token_secret)
 
 def open_cache():
     ''' opens the cache file if it exists and loads the JSON into
@@ -170,6 +202,12 @@ def construct_unique_key(baseurl, params):
     string
         the unique key as a string
     '''
+    if "key" in params:
+        del params["key"]
+    if "appid" in params:
+        del params["appid"]
+    print(params)
+
     param_strings = []
     connector = '_'
     for k in params.keys():
@@ -229,64 +267,10 @@ CACHE_DICT = open_cache()
 map_endpoint_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
 weather_endpoint_url = 'https://api.openweathermap.org/data/2.5/forecast'
 map_find_place_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+distance_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
 
 
-
-#results = make_request_with_cache(map_endpoint_url, params)
-# tweets = results['statuses']
-# for t in tweets:
-#     print(t['text'])
-
-mapTree = \
-    ("Do you want to enter your begining location? or directly search a park?",
-        ("Please type your beginning location", None, None),
-        ("Please type your search place", None, None))
-
-weatherTree = \
-    ("Please select your maximum acceptable cloud percentage",
-        ("Your cloud percentage is at most 20%", None, None),
-        ("Your cloud percentage is at most 10%", None, None))
-
-def ask_map(tree, answer, i):
-
-    if not tree[1]:
-        if type(tree[0]) != str:
-            return tree[0]
-        return "I will search for you."
-    else:
-        if answer[i] == "1":
-            return ask_map(tree[1], answer, i + 1)
-        else:
-            return ask_map(tree[2], answer, i + 1)
-
-
-
-    #     if not tree[1]:
-    #     prompt = f"Is it {tree[0]}? "
-    #     if not check_answer(prompt):
-    #         object_name = input('Drats! What was it? ')
-    #         question = input(f"What's a question that distinguishes between {object_name} and {tree[0]}? ")
-    #         answer = check_answer(f"And what's the answer for {object_name}? ")
-    #         if answer:
-    #             cur_tree = tree
-    #             tree = (question, (object_name, None, None), cur_tree)
-    #         else:
-    #             cur_tree = tree
-    #             tree = (question, cur_tree, (object_name, None, None))
-
-    #     else:
-    #         print("I got it!")
-    #     return tree
-    # else:
-    #     prompt = f"{tree[0]} "
-    #     if check_answer(prompt):
-    #         tree = (tree[0], play(tree[1]), tree[2])
-    #         return tree
-
-    #     else:
-    #         tree = (tree[0], tree[1], play(tree[2]))
-    #         return tree
 
 
 if __name__ == "__main__":
